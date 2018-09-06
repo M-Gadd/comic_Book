d22aa46195e5d78806ecea79ea9e42d62e45fc6b
Mostafa Gad <Gad.mostafa@gmail.com>
2018-09-06 15:47:46 +0200

    JS different HTML links

---
+++ javascript/script.js
@@ -0,0 +1,29 @@
+var ep1 = document.getElementById('ep1');
+ep1.onclick = function(){
+  window.location.href = './html/episode1.html';
+}
+
+var ep2 = document.getElementById('ep2');
+ep2.onclick = function(){
+  window.location.href = './html/episode2.html';
+}
+
+var ep3 = document.getElementById('ep3');
+ep3.onclick = function(){
+  window.location.href = './html/episode3.html';
+}
+
+var ep4 = document.getElementById('ep4');
+ep4.onclick = function(){
+  window.location.href = './html/episode4.html';
+}
+
+var ep5 = document.getElementById('ep5');
+ep5.onclick = function(){
+  window.location.href = './html/episode5.html';
+}
+
+var ep6 = document.getElementById('ep6');
+ep6.onclick = function(){
+  window.location.href = './html/episode6.html';
+}
