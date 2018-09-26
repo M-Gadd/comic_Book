e8b66c9eb1a9a3bebe94a135d1122f3491f69bfb
Lucas Montenoise <montenoise.lucas@gmail.com>
2018-09-26 17:05:18 +0200

    split the javascript files into categories and added a fade in effect on the episodes

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -12,6 +12,7 @@ episodes.forEach(one => {
   one.onclick = function(){
     isClicked = true;
     let id = one.getAttribute('id');
+    $( '.bg' ).hide();
     document.body.style.backgroundImage = `url('./images/homepage/homepage-gif-bcg.gif')`;
     setTimeout(() => {
       window.location.href = `./last-quest/episode${id}.html`;
@@ -124,79 +125,3 @@ allComics.forEach(one => {
     }
   }
 })
-
-
-
-
-// PARALLAX THINGY
-////////////////////////////////////////////////////////////////////
-
-
-
-// TOAD /////////////////////////////////
-var controller = new ScrollMagic.Controller({vertical: true});
-
-//parallax scene
-var slideParallaxScene = new ScrollMagic.Scene({
-    triggerElement: '.toad',
-    triggerHook: 1,
-    duration: '250%'
-})
-.setTween(TweenMax.from('.foreground-toad', 1, {y: '40%', ease:Power0.easeNone}))
-.addTo(controller)
-
-
-
-// GIANTS /////////////////////////////////
-var controller = new ScrollMagic.Controller({vertical: true});
-
-//parallax scene
-var slideParallaxScene = new ScrollMagic.Scene({
-    triggerElement: '.giants',
-    triggerHook: 1,
-    duration: '280%'
-})
-.setTween(TweenMax.from('.bcg-giants', 1, {y: '-40%', ease:Power0.easeNone}))
-.addTo(controller)
-
-
-
-// FIGHT /////////////////////////////////
-var controller = new ScrollMagic.Controller({vertical: true});
-
-//parallax scene
-var slideParallaxScene = new ScrollMagic.Scene({
-    triggerElement: '.fight',
-    triggerHook: 1,
-    duration: '150%'
-})
-.setTween(TweenMax.fromTo('.foreground-fight', 1, {y: '50%', ease:Power0.easeNone}, {y: '0%', ease:Power0.easeNone}))
-.addTo(controller)
-
-
-
-// RUINS /////////////////////////////////
-var controller = new ScrollMagic.Controller({vertical: true});
-
-//parallax scene
-var slideParallaxScene = new ScrollMagic.Scene({
-    triggerElement: '.ruins',
-    triggerHook: 1,
-    duration: '200%'
-})
-.setTween(TweenMax.fromTo('.bcg-ruins', 1, {y: '0%', ease:Power0.easeNone}, {y: '30%', ease:Power0.easeNone}))
-.addTo(controller)
-
-
-
-// GUARDS /////////////////////////////////
-var controller = new ScrollMagic.Controller({vertical: true});
-
-//parallax scene
-var slideParallaxScene = new ScrollMagic.Scene({
-    triggerElement: '.guards',
-    triggerHook: 1,
-    duration: '250%'
-})
-.setTween(TweenMax.fromTo('.foreground-guards', 1, {y: '30%', ease:Power0.easeNone}, {y: '0%', ease:Power0.easeNone}))
-.addTo(controller)
