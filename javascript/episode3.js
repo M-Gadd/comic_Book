8c50eacbc9da2c2d697dc4cadb1f21d74ae0d9f1
Lucas Montenoise <montenoise.lucas@gmail.com>
2018-10-11 14:50:09 +0200

    changed the header size and proportions according to ref visual

--- javascript/episode3.js
+++ javascript/episode3.js
@@ -17,29 +17,3 @@ $(window).on( "load", () => {
 
 $( document ).ready( function() {
 })
-
-////////////////////////////////////////////////////////////////////////////
-// PARALLAX THINGY
-////////////////////////////////////////////////////////////////////////////
-
-// var controller = new ScrollMagic.Controller({vertical: true});
-
-// // FOREST /////////////////////////////////
-
-// // build tween
-// var tween = new TimelineMax ()
-//   .add([
-//     TweenMax.fromTo('.forest-1', 1, {y: '85%', ease:Power0.easeNone},{y: '20%', ease:Power0.easeNone}),
-//     TweenMax.fromTo('.forest-2', 1, {y: '25%', ease:Power0.easeNone},{y: '-10%', ease:Power0.easeNone}),
-//     TweenMax.fromTo('.forest-3', 1, {y: '12%', ease:Power0.easeNone},{y: '-5%', ease:Power0.easeNone}),
-//     TweenMax.fromTo('.forest-4', 1, {y: '4%', ease:Power0.easeNone},{y: '-2%', ease:Power0.easeNone}),
-//   ]);
-
-// // build scene
-// var slideParallaxScene = new ScrollMagic.Scene({
-//     triggerElement: '.forest',
-//     triggerHook: 1,
-//     duration: '360%'
-// })
-// .setTween(tween)
-// .addTo(controller)
